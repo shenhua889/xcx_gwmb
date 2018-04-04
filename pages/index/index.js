9dc69e97159259883cd1db45a98aca2fd75c4cb3
shenhua889 <shenhua889@126.com>
2018-04-04 17:33:48 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,88 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    images:[
+      '../../image/swiper-1.jpg',
+      '../../image/swiper-2.jpg',
+      '../../image/swiper-3.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  gotoupdate: function () {
+    wx.navigateTo({
+      url: '../update/update',
+    })
+  },
+  gotocontact: function () {
+    wx.navigateTo({
+      url: '../contactus/contactus',
+    }) 
+  },
+  gotoitem1: function () {
+    wx.navigateTo({
+      url: '../items/item-1',
+    })
+  },
+  gotoitem2: function () {
+    wx.navigateTo({
+      url: '../items/item-2',
+    })
+  },
+  gotoitem3: function () {
+    wx.navigateTo({
+      url: '../items/item-3',
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
